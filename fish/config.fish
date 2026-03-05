22ea589dcb2fc9b4b738d92853c3d515b88e61df
Seigo Uchida <seigo@hey.com>
2026-03-05 13:07:34 +0900

    add config for mise + Codex

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,13 @@ end
 set -gx EDITOR vim
 set -gx GHQ_ROOT $HOME/ghq
 
-fish_add_path /usr/local/bin
 fish_add_path /opt/homebrew/bin
+# Keep Homebrew mise binary ahead of /opt/homebrew/bin to avoid shim mismatch
+# warnings in `mise doctor` across terminal environments.
+fish_add_path --move --prepend /opt/homebrew/opt/mise/bin
+fish_add_path /usr/local/bin
 
 starship init fish | source
+mise activate fish | source
+
+
